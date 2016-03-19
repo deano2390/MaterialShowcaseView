6dcef5604cad342652463889676581277c98154b
Sandro Simas <sandro.csimas@gmail.com>
2016-03-19 20:05:02 -0300

    Do not set content text alpha

--- library/src/main/java/uk/co/deanwild/materialshowcaseview/MaterialShowcaseView.java
+++ library/src/main/java/uk/co/deanwild/materialshowcaseview/MaterialShowcaseView.java
@@ -356,8 +356,7 @@ public class MaterialShowcaseView extends FrameLayout implements View.OnTouchLis
     }
 
     private void setTitleText(CharSequence contentText) {
-        if (mTitleTextView != null && !contentText.equals("")) {
-            mContentTextView.setAlpha(0.5F);
+        if (mTitleTextView != null) {
             mTitleTextView.setText(contentText);
         }
     }
