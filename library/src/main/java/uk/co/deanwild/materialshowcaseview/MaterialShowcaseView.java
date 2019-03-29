f2ffecfa1aedac12ca1741e9d61a3992ab0ce712
Dean Wild <dean.wild@arris.com>
2019-03-29 14:36:18 +0000

    seemingly more reliable way to get the navBar height

--- library/src/main/java/uk/co/deanwild/materialshowcaseview/MaterialShowcaseView.java
+++ library/src/main/java/uk/co/deanwild/materialshowcaseview/MaterialShowcaseView.java
@@ -302,7 +302,11 @@ public class MaterialShowcaseView extends FrameLayout implements View.OnTouchLis
              * If we're on lollipop then make sure we don't draw over the nav bar
              */
             if (!mRenderOverNav && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+
                 mBottomMargin = getSoftButtonsBarSizePort((Activity) getContext());
+
+
                 FrameLayout.LayoutParams contentLP = (LayoutParams) getLayoutParams();
 
                 if (contentLP != null && contentLP.bottomMargin != mBottomMargin)
@@ -939,20 +943,16 @@ public class MaterialShowcaseView extends FrameLayout implements View.OnTouchLis
         PrefsManager.resetAll(context);
     }
 
-    public static int getSoftButtonsBarSizePort(Activity activity) {
-        // getRealMetrics is only available with API 17 and +
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            DisplayMetrics metrics = new DisplayMetrics();
-            activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
-            int usableHeight = metrics.heightPixels;
-            activity.getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
-            int realHeight = metrics.heightPixels;
-            if (realHeight > usableHeight)
-                return realHeight - usableHeight;
-            else
-                return 0;
+
+    public int getSoftButtonsBarSizePort() {
+
+        int resourceId = getResources().getIdentifier("navigation_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            return getResources().getDimensionPixelSize(resourceId);
         }
+
         return 0;
+
     }
 
     private void setRenderOverNavigationBar(boolean mRenderOverNav) {
