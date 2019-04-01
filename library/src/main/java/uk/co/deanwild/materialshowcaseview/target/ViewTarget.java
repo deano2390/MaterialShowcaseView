2d037cad55e41268e3627947a7ba95733b81ddb3
Louis Kirsch <louis@louiskirsch.com>
2019-04-01 17:47:02 +0200

    Handle context wrappers

--- library/src/main/java/uk/co/deanwild/materialshowcaseview/target/ViewTarget.java
+++ library/src/main/java/uk/co/deanwild/materialshowcaseview/target/ViewTarget.java
@@ -1,6 +1,8 @@
 package uk.co.deanwild.materialshowcaseview.target;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.view.View;
@@ -22,10 +24,21 @@ public class ViewTarget implements Target {
     public Point getPoint() {
         int[] location = new int[2];
         mView.getLocationInWindow(location);
-        View decor = ((Activity) mView.getContext()).getWindow().getDecorView();
+        View decor = getActivity(mView).getWindow().getDecorView();
         int x = location[0] - decor.getPaddingLeft() + mView.getWidth() / 2;
         int y = location[1] - decor.getPaddingTop() + mView.getHeight() / 2;
         return new Point(x, y);
+    }
+
+    private Activity getActivity(View view) {
+        Context context = view.getContext();
+        while (context instanceof ContextWrapper) {
+            if (context instanceof Activity) {
+                return (Activity)context;
+            }
+            context = ((ContextWrapper)context).getBaseContext();
+        }
+        return null;
     }
 
     @Override
